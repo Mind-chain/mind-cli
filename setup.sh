a8cbaf77b8291b61d4240aa44101b49f3e55306b
nnlgsakib <nlgarts@outlook.com>
2024-07-12 20:53:46 +0600

    added sudo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 wget https://github.com/Mind-chain/mind-cli/releases/download/v1.0.0/mind-cli
 
-chmod +x mind-cli
+sudo chmod +x mind-cli
 
-mv mind-cli /usr/bin
+sudo mv mind-cli /usr/bin
 mind-cli version
