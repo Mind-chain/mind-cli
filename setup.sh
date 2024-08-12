e5af0549ccb1620518759534daf40abc4ccef37c
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-08-12 16:38:31 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+validator_key=$(cat data/consensus/validator.key)
 wget https://github.com/Mind-chain/mind-cli/releases/download/v1.0.1/mind-cli
 
 sudo chmod +x mind-cli
@@ -5,8 +6,7 @@ sudo chmod +x mind-cli
 #sudo mv mind-cli /usr/bin
 ./mind-cli version
 ./mind-cli node  install-node
-
-./mind-cli node get-genesis
 ./mind-cli node init-node 
 validator_key=$(cat data/consensus/validator.key)
+./mind-cli node get-genesis
 echo "Privatkey: 0x$validator_key"
