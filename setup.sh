ce02d91c06732ed80046f214e5a1a9bc1196b297
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-08-12 16:16:58 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo chmod +x mind-cli
 
 #sudo mv mind-cli /usr/bin
 ./mind-cli version
-./mind-cli install-node
+./mind-cli node  install-node
 
 ./mind-cli node get-genesis
 ./mind-cli node init-node 
