7e8720175ed1c7442f1ed264c12d752adc14597d
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-08-12 09:51:19 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ wget https://github.com/Mind-chain/mind-cli/releases/download/v1.0.0/mind-cli
 
 sudo chmod +x mind-cli
 
-sudo mv mind-cli /usr/bin
-mind-cli version
+#sudo mv mind-cli /usr/bin
+#mind-cli version
