0d015209ca3b45b9597945d644a11712a80f7acc
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-08-26 01:09:04 +0600

    Update setup-service.sh

--- setup-service.sh
+++ setup-service.sh
@@ -40,7 +40,6 @@ sudo systemctl enable $SERVICE_NAME
 
 # Display the service status
 echo "Displaying the status of $SERVICE_NAME..."
-sudo systemctl status $SERVICE_NAME
 
 echo "Setup complete!"
 
